5a960f1bd3f5140ef56e1f8cc5c3f94ed50342ed
ssbuild <462304@qq.cn>
2022-12-05 09:29:06 +0800

    初始化tplinker,tpinker_plus
    Signed-off-by: ssbuild <462304@qq.cn>

--- deep_training/model/nlp/layers/norm.py
+++ deep_training/model/nlp/layers/norm.py
@@ -4,6 +4,96 @@
 import torch
 from torch import nn
 
+__all__ = [
+    'LayerNorm',
+    'LayerNorm2'
+]
+
+class LayerNorm2(nn.Module):
+    def __init__(self, input_dim, cond_dim=0, center=True, scale=True, epsilon=None, conditional=False,
+                 hidden_units=None, hidden_activation='linear', hidden_initializer='xaiver', **kwargs):
+        super(LayerNorm2, self).__init__()
+        """
+        input_dim: inputs.shape[-1]
+        cond_dim: cond.shape[-1]
+        """
+        self.center = center
+        self.scale = scale
+        self.conditional = conditional
+        self.hidden_units = hidden_units
+        # self.hidden_activation = activations.get(hidden_activation) keras中activation为linear时，返回原tensor,unchanged
+        self.hidden_initializer = hidden_initializer
+        self.epsilon = epsilon or 1e-12
+        self.input_dim = input_dim
+        self.cond_dim = cond_dim
+
+        if self.center:
+            self.beta = nn.Parameter(torch.zeros(input_dim))
+        if self.scale:
+            self.gamma = nn.Parameter(torch.ones(input_dim))
+
+        if self.conditional:
+            if self.hidden_units is not None:
+                self.hidden_dense = nn.Linear(in_features=self.cond_dim, out_features=self.hidden_units, bias=False)
+            if self.center:
+                self.beta_dense = nn.Linear(in_features=self.cond_dim, out_features=input_dim, bias=False)
+            if self.scale:
+                self.gamma_dense = nn.Linear(in_features=self.cond_dim, out_features=input_dim, bias=False)
+
+        self.initialize_weights()
+
+    def initialize_weights(self):
+
+        if self.conditional:
+            if self.hidden_units is not None:
+                if self.hidden_initializer == 'normal':
+                    torch.nn.init.normal(self.hidden_dense.weight)
+                elif self.hidden_initializer == 'xavier':  # glorot_uniform
+                    torch.nn.init.xavier_uniform_(self.hidden_dense.weight)
+            # 下面这两个为什么都初始化为0呢?
+            # 为了防止扰乱原来的预训练权重，两个变换矩阵可以全零初始化（单层神经网络可以用全零初始化，连续的多层神经网络才不应当用全零初始化），这样在初始状态，模型依然保持跟原来的预训练模型一致。
+            if self.center:
+                torch.nn.init.constant_(self.beta_dense.weight, 0)
+            if self.scale:
+                torch.nn.init.constant_(self.gamma_dense.weight, 0)
+
+    def forward(self, inputs, cond=None):
+        """
+            如果是条件Layer Norm，则cond不是None
+        """
+        if self.conditional:
+            if self.hidden_units is not None:
+                cond = self.hidden_dense(cond)
+            # for _ in range(K.ndim(inputs) - K.ndim(cond)): # K.ndim: 以整数形式返回张量中的轴数。
+            # TODO: 这两个为什么有轴数差呢？ 为什么在 dim=1 上增加维度??
+            # 为了保持维度一致，cond可以是（batch_size, cond_dim）
+            for _ in range(len(inputs.shape) - len(cond.shape)):
+                cond = cond.unsqueeze(1)  # cond = K.expand_dims(cond, 1)
+
+            # cond在加入beta和gamma之前做一次线性变换，以保证与input维度一致
+            if self.center:
+                beta = self.beta_dense(cond) + self.beta
+            if self.scale:
+                gamma = self.gamma_dense(cond) + self.gamma
+        else:
+            if self.center:
+                beta = self.beta
+            if self.scale:
+                gamma = self.gamma
+
+        outputs = inputs
+        if self.center:
+            mean = torch.mean(outputs, dim=-1).unsqueeze(-1)
+            outputs = outputs - mean
+        if self.scale:
+            variance = torch.mean(outputs ** 2, dim=-1).unsqueeze(-1)
+            std = (variance + self.epsilon) ** 2
+            outputs = outputs / std
+            outputs = outputs * gamma
+        if self.center:
+            outputs = outputs + beta
+
+        return outputs
 
 class LayerNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-12, conditional_size=None, weight=True, bias=True, norm_mode='normal',
