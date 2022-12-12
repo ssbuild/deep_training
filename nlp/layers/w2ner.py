db7fe7b79032d45a394b376e88cf6807540a167c
passion765 <462304@qq.com>
2022-12-12 23:36:14 +0800

    w2ner初始化
    Signed-off-by: passion765 <462304@qq.com>

---
+++ nlp/layers/w2ner.py
@@ -0,0 +1,101 @@
+# @Time    : 2022/12/12 23:19
+# @Author  : tk
+# @FileName: w2ner.py
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class ConvolutionLayer(nn.Module):
+    def __init__(self, input_size, channels, dilation, dropout=0.1):
+        super(ConvolutionLayer, self).__init__()
+        self.base = nn.Sequential(
+            nn.Dropout2d(dropout),
+            nn.Conv2d(input_size, channels, kernel_size=1),
+            nn.GELU(),
+        )
+
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(channels, channels, kernel_size=3, groups=channels, dilation=d, padding=d) for d in dilation])
+
+    def forward(self, x):
+        x = x.permute(0, 3, 1, 2).contiguous()
+        x = self.base(x)
+
+        outputs = []
+        for conv in self.convs:
+            x = conv(x)
+            x = F.gelu(x)
+            outputs.append(x)
+        outputs = torch.cat(outputs, dim=1)
+        outputs = outputs.permute(0, 2, 3, 1).contiguous()
+        return outputs
+
+
+class Biaffine(nn.Module):
+    def __init__(self, n_in, n_out=1, bias_x=True, bias_y=True):
+        super(Biaffine, self).__init__()
+
+        self.n_in = n_in
+        self.n_out = n_out
+        self.bias_x = bias_x
+        self.bias_y = bias_y
+        weight = torch.zeros((n_out, n_in + int(bias_x), n_in + int(bias_y)))
+        nn.init.xavier_normal_(weight)
+        self.weight = nn.Parameter(weight, requires_grad=True)
+
+    def extra_repr(self):
+        s = f"n_in={self.n_in}, n_out={self.n_out}"
+        if self.bias_x:
+            s += f", bias_x={self.bias_x}"
+        if self.bias_y:
+            s += f", bias_y={self.bias_y}"
+
+        return s
+
+    def forward(self, x, y):
+        if self.bias_x:
+            x = torch.cat((x, torch.ones_like(x[..., :1])), -1)
+        if self.bias_y:
+            y = torch.cat((y, torch.ones_like(y[..., :1])), -1)
+        # [batch_size, n_out, seq_len, seq_len]
+        s = torch.einsum('bxi,oij,byj->boxy', x, self.weight, y)
+        # remove dim 1 if n_out == 1
+        s = s.permute(0, 2, 3, 1)
+
+        return s
+
+
+class MLP(nn.Module):
+    def __init__(self, n_in, n_out, dropout=0):
+        super().__init__()
+
+        self.linear = nn.Linear(n_in, n_out)
+        self.activation = nn.GELU()
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        x = self.dropout(x)
+        x = self.linear(x)
+        x = self.activation(x)
+        return x
+
+
+class CoPredictor(nn.Module):
+    def __init__(self, cls_num, hid_size, biaffine_size, channels, ffnn_hid_size, dropout=0):
+        super().__init__()
+        self.mlp1 = MLP(n_in=hid_size, n_out=biaffine_size, dropout=dropout)
+        self.mlp2 = MLP(n_in=hid_size, n_out=biaffine_size, dropout=dropout)
+        self.biaffine = Biaffine(n_in=biaffine_size, n_out=cls_num, bias_x=True, bias_y=True)
+        self.mlp_rel = MLP(channels, ffnn_hid_size, dropout=dropout)
+        self.linear = nn.Linear(ffnn_hid_size, cls_num)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, y, z):
+        h = self.dropout(self.mlp1(x))
+        t = self.dropout(self.mlp2(y))
+        o1 = self.biaffine(h, t)
+
+        z = self.dropout(self.mlp_rel(z))
+        o2 = self.linear(z)
+        return o1 + o2
--- nlp/models/w2ner.py
+++ nlp/models/w2ner.py
@@ -4,150 +4,174 @@
 '''
 参考: https://github.com/ljynlp/W2NER
 '''
+import typing
+from dataclasses import dataclass, field
+
 import torch
 import torch.nn.functional as F
+from deep_training.nlp.models.transformer import TransformerModel
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from ..layers.norm import LayerNorm
+from ..layers.w2ner import MLP,CoPredictor,ConvolutionLayer,Biaffine
 
-from nlp.layers.norm import LayerNorm
+@dataclass
+class W2nerArguments:
+    use_bert_last_4_layers: typing.Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    dist_emb_size: typing.Optional[int] = field(
+        default=20,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    type_emb_size: typing.Optional[int] = field(
+        default=20,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    lstm_hid_size: typing.Optional[int] = field(
+        default=768,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
 
-class ConvolutionLayer(nn.Module):
-    def __init__(self, input_size, channels, dilation, dropout=0.1):
-        super(ConvolutionLayer, self).__init__()
-        self.base = nn.Sequential(
-            nn.Dropout2d(dropout),
-            nn.Conv2d(input_size, channels, kernel_size=1),
-            nn.GELU(),
-        )
+    conv_hid_size: typing.Optional[int] = field(
+        default=96,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    biaffine_size: typing.Optional[int] = field(
+        default=768,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    ffnn_hid_size: typing.Optional[int] = field(
+        default=128,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    dilation: typing.Optional[tuple] = field(
+        default=(1,2,3),
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
 
-        self.convs = nn.ModuleList(
-            [nn.Conv2d(channels, channels, kernel_size=3, groups=channels, dilation=d, padding=d) for d in dilation])
-
-    def forward(self, x):
-        x = x.permute(0, 3, 1, 2).contiguous()
-        x = self.base(x)
-
-        outputs = []
-        for conv in self.convs:
-            x = conv(x)
-            x = F.gelu(x)
-            outputs.append(x)
-        outputs = torch.cat(outputs, dim=1)
-        outputs = outputs.permute(0, 2, 3, 1).contiguous()
-        return outputs
-
-
-class Biaffine(nn.Module):
-    def __init__(self, n_in, n_out=1, bias_x=True, bias_y=True):
-        super(Biaffine, self).__init__()
-
-        self.n_in = n_in
-        self.n_out = n_out
-        self.bias_x = bias_x
-        self.bias_y = bias_y
-        weight = torch.zeros((n_out, n_in + int(bias_x), n_in + int(bias_y)))
-        nn.init.xavier_normal_(weight)
-        self.weight = nn.Parameter(weight, requires_grad=True)
-
-    def extra_repr(self):
-        s = f"n_in={self.n_in}, n_out={self.n_out}"
-        if self.bias_x:
-            s += f", bias_x={self.bias_x}"
-        if self.bias_y:
-            s += f", bias_y={self.bias_y}"
-
-        return s
-
-    def forward(self, x, y):
-        if self.bias_x:
-            x = torch.cat((x, torch.ones_like(x[..., :1])), -1)
-        if self.bias_y:
-            y = torch.cat((y, torch.ones_like(y[..., :1])), -1)
-        # [batch_size, n_out, seq_len, seq_len]
-        s = torch.einsum('bxi,oij,byj->boxy', x, self.weight, y)
-        # remove dim 1 if n_out == 1
-        s = s.permute(0, 2, 3, 1)
-
-        return s
-
-
-class MLP(nn.Module):
-    def __init__(self, n_in, n_out, dropout=0):
-        super().__init__()
-
-        self.linear = nn.Linear(n_in, n_out)
-        self.activation = nn.GELU()
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-        x = self.dropout(x)
-        x = self.linear(x)
-        x = self.activation(x)
-        return x
+    emb_dropout: typing.Optional[float] = field(
+        default=0.5,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    conv_dropout: typing.Optional[float] = field(
+        default=0.5,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
+    out_dropout: typing.Optional[float] = field(
+        default=0.3333,
+        metadata={
+            "help": (
+                ""
+            )
+        },
+    )
 
 
-class CoPredictor(nn.Module):
-    def __init__(self, cls_num, hid_size, biaffine_size, channels, ffnn_hid_size, dropout=0):
-        super().__init__()
-        self.mlp1 = MLP(n_in=hid_size, n_out=biaffine_size, dropout=dropout)
-        self.mlp2 = MLP(n_in=hid_size, n_out=biaffine_size, dropout=dropout)
-        self.biaffine = Biaffine(n_in=biaffine_size, n_out=cls_num, bias_x=True, bias_y=True)
-        self.mlp_rel = MLP(channels, ffnn_hid_size, dropout=dropout)
-        self.linear = nn.Linear(ffnn_hid_size, cls_num)
-        self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, y, z):
-        h = self.dropout(self.mlp1(x))
-        t = self.dropout(self.mlp2(y))
-        o1 = self.biaffine(h, t)
 
-        z = self.dropout(self.mlp_rel(z))
-        o2 = self.linear(z)
-        return o1 + o2
+def extract_lse(outputs,id2seqs):...
+    # batch_result = []
+    # for crf_logits, ents_logits in zip(outputs[0],outputs[1].argmax(-1)):
+    #     batch_result.append(get_entities([id2seqs[l] for l in crf_logits], ents_logits))
+    # return batch_result
 
-class Model(nn.Module):
-    def __init__(self, config):
-        super(Model, self).__init__()
-        self.use_bert_last_4_layers = config.use_bert_last_4_layers
 
-        self.lstm_hid_size = config.lstm_hid_size
-        self.conv_hid_size = config.conv_hid_size
+class TransformerForW2ner(TransformerModel):
+    def __init__(self,w2nerArguments: W2nerArguments, *args,**kwargs):
+        super(TransformerForW2ner, self).__init__(*args,**kwargs)
+       
+        config = self.config
+        self.cross_loss = nn.CrossEntropyLoss(reduction='none')
+        self.lstm_hid_size = w2nerArguments.lstm_hid_size
+        self.conv_hid_size = w2nerArguments.conv_hid_size
 
         lstm_input_size = 0
+        lstm_input_size += config.hidden_size
 
-        self.bert = AutoModel.from_pretrained(config.bert_name, cache_dir="./cache/", output_hidden_states=True)
-        lstm_input_size += config.bert_hid_size
+        self.dis_embs = nn.Embedding(20, w2nerArguments.dist_emb_size)
+        self.reg_embs = nn.Embedding(3, w2nerArguments.type_emb_size)
 
-        self.dis_embs = nn.Embedding(20, config.dist_emb_size)
-        self.reg_embs = nn.Embedding(3, config.type_emb_size)
-
-        self.encoder = nn.LSTM(lstm_input_size, config.lstm_hid_size // 2, num_layers=1, batch_first=True,
+        self.encoder = nn.LSTM(lstm_input_size, w2nerArguments.lstm_hid_size // 2, num_layers=1, batch_first=True,
                                bidirectional=True)
 
-        conv_input_size = config.lstm_hid_size + config.dist_emb_size + config.type_emb_size
+        conv_input_size = w2nerArguments.lstm_hid_size + w2nerArguments.dist_emb_size + w2nerArguments.type_emb_size
 
-        self.convLayer = ConvolutionLayer(conv_input_size, config.conv_hid_size, config.dilation, config.conv_dropout)
-        self.dropout = nn.Dropout(config.emb_dropout)
-        self.predictor = CoPredictor(config.label_num, config.lstm_hid_size, config.biaffine_size,
-                                     config.conv_hid_size * len(config.dilation), config.ffnn_hid_size,
-                                     config.out_dropout)
+        self.convLayer = ConvolutionLayer(conv_input_size, w2nerArguments.conv_hid_size, w2nerArguments.dilation, w2nerArguments.conv_dropout)
+        self.dropout = nn.Dropout(w2nerArguments.emb_dropout)
+        self.predictor = CoPredictor(config.num_labels, 
+                                     w2nerArguments.lstm_hid_size, 
+                                     w2nerArguments.biaffine_size,
+                                     w2nerArguments.conv_hid_size * len(w2nerArguments.dilation), 
+                                     w2nerArguments.ffnn_hid_size,
+                                     w2nerArguments.out_dropout)
 
         self.cln = LayerNorm(config.lstm_hid_size, config.lstm_hid_size, conditional=True)
 
-    def forward(self, bert_inputs, grid_mask2d, dist_inputs, pieces2word, sent_length):
+
+    def get_model_lr(self):
+        return super(TransformerForW2ner, self).get_model_lr() + [
+            (self.dropout, self.config.task_specific_params['learning_rate_for_task']),
+            (self.seqs_classifier, self.config.task_specific_params['learning_rate_for_task']),
+            (self.ents_classifier, self.config.task_specific_params['learning_rate_for_task']),
+            (self.crf, self.config.task_specific_params['learning_rate_for_task']),
+        ]
+
+    def mlp(self, bert_embs, grid_mask2d, dist_inputs, pieces2word, sent_length):
         '''
-        :param bert_inputs: [B, L']
+        # :param bert_inputs: [B, L']
         :param grid_mask2d: [B, L, L]
         :param dist_inputs: [B, L, L]
         :param pieces2word: [B, L, L']
         :param sent_length: [B]
         :return:
         '''
-        bert_embs = self.bert(input_ids=bert_inputs, attention_mask=bert_inputs.ne(0).float())
-        if self.use_bert_last_4_layers:
-            bert_embs = torch.stack(bert_embs[2][-4:], dim=-1).mean(-1)
-        else:
-            bert_embs = bert_embs[0]
+        # bert_embs = self.bert(input_ids=bert_inputs, attention_mask=bert_inputs.ne(0).float())
+        # if self.use_bert_last_4_layers:
+        #     bert_embs = torch.stack(bert_embs[2][-4:], dim=-1).mean(-1)
+        # else:
+        #     bert_embs = bert_embs[0]
 
         length = pieces2word.size(1)
 
@@ -175,5 +199,35 @@ class Model(nn.Module):
         conv_outputs = self.convLayer(conv_inputs)
         conv_outputs = torch.masked_fill(conv_outputs, grid_mask2d.eq(0).unsqueeze(-1), 0.0)
         outputs = self.predictor(word_reps, word_reps, conv_outputs)
-
         return outputs
+
+    def compute_loss(self,batch,batch_idx):
+        labels: torch.Tensor = batch.pop('labels',None)
+
+        if labels is not None:
+            grid_mask2d= batch.pop('grid_mask2d',None)
+            dist_inputs= batch.pop('dist_inputs',None)
+            pieces2word= batch.pop('pieces2word',None)
+            sent_length= batch.pop('sent_length',None)
+        attention_mask = batch['attention_mask']
+        outputs = self(**batch)
+        logits = outputs[0]
+        if self.model.training:
+            logits = self.dropout(logits)
+
+        if labels is not None:
+            seqs_labels = torch.where(seqs_labels >= 0, seqs_labels, torch.zeros_like(seqs_labels))
+            loss1 = self.crf(emissions=seqs_logits, tags=seqs_labels, mask=attention_mask)
+            loss2 = self.cross_loss(ents_logits.view(-1,ents_logits.shape[-1]),ents_labels.view(-1))
+            attention_mask = attention_mask.float().view(-1)
+            loss2 = (loss2 * attention_mask).sum() / (attention_mask.sum() + 1e-12)
+            loss_dict = {
+                'crf_loss': loss1,
+                'ents_loss': loss2,
+                'loss': loss1+ loss2
+            }
+            outputs = (loss_dict,crf_tags,ents_logits,seqs_labels,ents_labels)
+        else:
+            outputs = (crf_tags,ents_logits)
+        return outputs
+
