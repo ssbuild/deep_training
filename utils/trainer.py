2804f290d12c184124a82f64ac1a4e716111bae4
ssbuild <462304@qq.cn>
2022-12-22 09:08:05 +0800

    补丁
    Signed-off-by: ssbuild <462304@qq.cn>

--- utils/trainer.py
+++ utils/trainer.py
@@ -14,16 +14,17 @@ __all__ = [
 
 class CheckpointCallback(Checkpoint):
     def __init__(self,
+                 rank=0,# 执行节点
                  every_n_train_steps: Optional[int] = None,
                  every_n_epochs: Optional[int] = 1,
                  monitor='val_f1'):
-        self.__steps = 0
-        self.__epochs = 0
+
         self.__every_n_train_steps = every_n_train_steps
         self.__every_n_epochs = every_n_epochs
         assert not (self.__every_n_epochs is None and self.__every_n_train_steps is None)
         self.best = {}
         self.monitor = monitor
+        self.rank = rank
 
 
     def on_save_model(
@@ -34,6 +35,11 @@ class CheckpointCallback(Checkpoint):
     def __on_save_model(
         self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
     ) -> None:
+
+        if trainer.world_size > 1:
+            if self.rank >= 0 and trainer.global_rank != self.rank:
+                return
+
         pl_module.eval()
         with torch.no_grad():
             self.on_save_model(trainer,pl_module)
@@ -49,9 +55,9 @@ class CheckpointCallback(Checkpoint):
             The value ``outputs["loss"]`` here will be the normalized value w.r.t ``accumulate_grad_batches`` of the
             loss returned from ``training_step``.
         """
-        self.__steps += 1
+
         if self.__every_n_train_steps is not None and self.__every_n_train_steps > 0:
-            if self.__steps % self.__every_n_train_steps == 0:
+            if trainer.global_step % self.__every_n_train_steps == 0:
                 self.__on_save_model(trainer,pl_module)
 
 
@@ -64,9 +70,9 @@ class CheckpointCallback(Checkpoint):
         1. Implement `training_epoch_end` in the `LightningModule` and access outputs via the module OR
         2. Cache data across train batch hooks inside the callback implementation to post-process in this hook.
         """
-        self.__epochs += 1
+
 
         if self.__every_n_epochs is not None and self.__every_n_epochs > 0:
-            if self.__epochs % self.__every_n_epochs == 0:
+            if trainer.current_epoch % self.__every_n_epochs == 0:
                 self.__on_save_model(trainer, pl_module)
 
