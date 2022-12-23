eba110aea51240c55e56b170cb192c029db3705f
ssbuild <462304@qq.cn>
2022-12-23 17:26:15 +0800

    增加事件抽取
    Signed-off-by: ssbuild <462304@qq.cn>

--- nlp/models/gplinker.py
+++ nlp/models/gplinker.py
@@ -2,6 +2,8 @@
 # @Author  : tk
 # @FileName: gp_linker.py
 import typing
+from itertools import groupby
+
 import numpy as np
 import torch
 from torch import nn
@@ -86,21 +88,118 @@ class TransformerForGplinker(TransformerModel):
 
 
 
-# def extract_spoes_from_labels(outputs: typing.List):
-#     batch_spoes = []
-#     for l1, l2, l3 in zip(outputs[0], outputs[1], outputs[2]):
-#         subjects, objects = set(), set()
-#         for p,es in enumerate(l1):
-#             o = subjects if p == 0 else objects
-#             for e in es:
-#                 if e[0] != 0 and e[1] != 0:
-#                     o.add((e[0], e[1]))
-#
-#         spoes = set()
-#         for p,(hs,ts) in enumerate(zip(l2,l3)):
-#             for h in hs:
-#                 for t in ts:
-#                     if (h[0],t[0]) in subjects and (h[1],t[1]) in objects:
-#                         spoes.add((h[0] - 1, t[0] - 1, p, h[1] - 1, t[1] - 1))
-#         batch_spoes.append(list(spoes))
-#     return batch_spoes
+
+class DedupList(list):
+    """定义去重的list
+    """
+    def append(self, x):
+        if x not in self:
+            super(DedupList, self).append(x)
+
+
+def neighbors(host, argus, links):
+    """构建邻集（host节点与其所有邻居的集合）
+    """
+    results = [host]
+    for argu in argus:
+        if host[1:] + argu[1:] in links:
+            results.append(argu)
+    return list(sorted(results))
+
+def clique_search(argus, links):
+    """搜索每个节点所属的完全子图作为独立事件
+    搜索思路：找出不相邻的节点，然后分别构建它们的邻集，递归处理。
+    """
+    Argus = DedupList()
+    for i1, (_,  h1, t1) in enumerate(argus):
+        for i2, (_,  h2, t2) in enumerate(argus):
+            if i2 > i1:
+                if (h1, t1, h2, t2) not in links:
+                    Argus.append(neighbors(argus[i1], argus, links))
+                    Argus.append(neighbors(argus[i2], argus, links))
+    if Argus:
+        results = DedupList()
+        for A in Argus:
+            for a in clique_search(A, links):
+                results.append(a)
+        return results
+    else:
+        return [list(sorted(argus))]
+
+
+def extract_events(outputs,threshold=1e-8,trigger=True):
+    for entities,heads,tails in zip(outputs[0],outputs[1],outputs[2]):
+        # 抽取论元
+        argus = set()
+        entities[:, [0, -1]] -= np.inf
+        entities[:, :, [0, -1]] -= np.inf
+        for l, h, t in zip(*np.where(entities > threshold)):
+            argus.add((l,h,t))
+        # 构建链接
+        links = set()
+        for i1, (_, h1, t1) in enumerate(argus):
+            for i2, (_, h2, t2) in enumerate(argus):
+                if i2 > i1:
+                    if heads[0, min(h1, h2), max(h1, h2)] > threshold:
+                        if tails[0, min(t1, t2), max(t1, t2)] > threshold:
+                            links.add((h1, t1, h2, t2))
+                            links.add((h2, t2, h1, t1))
+        # 析出事件
+        events = []
+        for _, sub_argus in groupby(sorted(argus), key=lambda s: s[0]):
+            for event in clique_search(list(sub_argus), links):
+                events.append([])
+                for argu in event:
+                    events[-1].append(argu[:1] + (argu[1],argu[2]))
+                    # start, end = mapping[argu[2]][0], mapping[argu[3]][-1] + 1
+                    # events[-1].append(argu[:2] + (text[start:end], start))
+                if trigger and all([argu[0] != u'触发词' for argu in event]):
+                    events.pop()
+
+
+class TransformerForGplinkerEvent(TransformerModel):
+    def __init__(self,  *args, **kwargs):
+        with_efficient = kwargs.pop('with_efficient',True)
+        super(TransformerForGplinkerEvent, self).__init__(*args, **kwargs)
+        self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
+        PointerLayerObject = EfficientPointerLayer if with_efficient else PointerLayer
+        self.entities_layer = PointerLayerObject(self.config.hidden_size, self.config.num_labels, 64)
+        self.heads_layer = PointerLayerObject(self.config.hidden_size, 1, 64, RoPE=False)
+        self.tails_layer = PointerLayerObject(self.config.hidden_size, 1, 64, RoPE=False)
+
+    def get_model_lr(self):
+        return super(TransformerForGplinkerEvent, self).get_model_lr() + [
+            (self.entities_layer, self.config.task_specific_params['learning_rate_for_task']),
+            (self.heads_layer, self.config.task_specific_params['learning_rate_for_task']),
+            (self.tails_layer, self.config.task_specific_params['learning_rate_for_task']),
+        ]
+
+    def compute_loss(self, *args,**batch) -> tuple:
+        entity_labels: torch.Tensor = batch.pop('entity_labels', None)
+        head_labels: torch.Tensor = batch.pop('head_labels', None)
+        tail_labels: torch.Tensor = batch.pop('tail_labels', None)
+        attention_mask = batch['attention_mask']
+        outputs = self.model(*args,**batch)
+        logits = outputs[0]
+        if self.model.training:
+            logits = self.dropout(logits)
+        logits1 = self.entities_layer(logits, attention_mask)
+        logits2 = self.heads_layer(logits, attention_mask)
+        logits3 = self.tails_layer(logits, attention_mask)
+        if entity_labels is not None:
+            loss1 = loss_for_gplinker(entity_labels, logits1)
+            loss2 = loss_for_gplinker(head_labels, logits2)
+            loss3 = loss_for_gplinker(tail_labels, logits3)
+            loss = (loss1 + loss2 + loss3) / 3
+            loss_dict = {'loss': loss,
+                         'loss_entities': loss1,
+                         'loss_head': loss2,
+                         'loss_tail': loss3}
+            outputs = (loss_dict, logits1, logits2, logits3,
+                       entity_labels, head_labels, tail_labels)
+        else:
+            outputs = (logits1, logits2, logits3)
+        return outputs
+
+
+
