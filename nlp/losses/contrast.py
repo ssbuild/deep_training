e0355f4bf12cc10de8aed4944ed932b90ff73e4b
passion765 <462304@qq.com>
2023-01-12 22:50:59 +0800

    2023年1月12日22:51:03
    Signed-off-by: passion765 <462304@qq.com>

--- nlp/losses/contrast.py
+++ nlp/losses/contrast.py
@@ -6,16 +6,15 @@ import torch
 from torch.nn.functional import cross_entropy
 
 
-def compute_simcse_loss(y_pred: torch.Tensor):
+def compute_simcse_loss(y_pred: torch.Tensor,scale = 20):
     batch_size = y_pred.size()[0]
-    idxs = torch.arange(0, batch_size )
-    idxs_1 = idxs[None, :]
-    idxs_2 = (idxs + 1 - idxs % 2 * 2)[:, None]
-    labels = torch.eq(idxs_1, idxs_2).to(y_pred.device).float()
-    y_pred = torch.layer_norm(y_pred,normalized_shape=y_pred.shape)
+    idxs = torch.arange(0, batch_size)
+    idxs = torch.reshape(idxs, (-1, 2))
+    idxs = torch.cat([idxs[:, 1:], idxs[:, :1]], dim=1)
+    labels = torch.reshape(idxs, (-1,)).to(y_pred.device).long()
     similarities = torch.mm(y_pred, torch.transpose(y_pred,1,0))
     similarities = similarities - torch.eye(batch_size).to(y_pred.device) * 1e12
-    similarities = similarities * 30  # scale
+    similarities = similarities * scale  # scale
     loss = cross_entropy(similarities, labels)
     return loss
 
