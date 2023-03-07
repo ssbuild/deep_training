3c8020c0c36874668544115d51c642b3453161fd
ssbuild <462304@qq.cn>
2023-03-07 17:16:01 +0800

    初始化keras
    Signed-off-by: ssbuild <462304@qq.cn>

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
                       'tfrecords >= 0.2.4 , <=3',
                       'sentencepiece',
                       'numpy',
-                      'transformers','seqmetric','sklearn','scipy','scikit-learn','tqdm','six'],
+                      'transformers >= 4.16',
+                      'seqmetric','sklearn','scipy','scikit-learn','tqdm','six'],
     packages=[p for p in find_packages() if p not in ignore]
 )
