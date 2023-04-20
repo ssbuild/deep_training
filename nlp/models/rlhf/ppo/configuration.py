3b9825f26bee56584241d86bdb0b250febd89a56
ssbuild <462304@qq.cn>
2023-04-20 17:25:36 +0800

    初始化ppo
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ nlp/models/rlhf/ppo/configuration.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/20 11:03
+import warnings
+from dataclasses import field, dataclass
+from typing import Optional
+
+import numpy as np
+from transformers.utils import flatten_dict
+
