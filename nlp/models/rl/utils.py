f4f81497aeb7762e09814740f41029a14fb95361
ssbuild <462304@qq.cn>
2023-05-11 10:44:43 +0800

    0.1.5 rc0
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ nlp/models/rl/utils.py
@@ -0,0 +1,210 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/5/11 9:07
+import functools
+from dataclasses import dataclass
+from typing import Optional, Tuple, Union
+import torch
+from torch import nn
+from transformers import PretrainedConfig
+from transformers.utils import ModelOutput
+
+
+@dataclass
+class CausalLMOutputWithValue(ModelOutput):
+    loss: Optional[torch.FloatTensor] = None
+    logits: Optional[torch.FloatTensor] = None
+    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    cross_attentions: Optional[Tuple[torch.FloatTensor]] = None
+    value: Optional[torch.FloatTensor] = None
+
+@dataclass
+class CausalPrefixLMOutputWithValue(ModelOutput):
+    loss: Optional[torch.FloatTensor] = None
+    logits: torch.FloatTensor = None
+    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    value: Optional[torch.FloatTensor] = None
+
+@dataclass
+class Seq2SeqLMOutputWithValue(ModelOutput):
+    loss: Optional[torch.FloatTensor] = None
+    logits: Optional[torch.FloatTensor] = None
+    past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None
+    decoder_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    decoder_attentions: Optional[Tuple[torch.FloatTensor]] = None
+    cross_attentions: Optional[Tuple[torch.FloatTensor]] = None
+    encoder_last_hidden_state: Optional[torch.FloatTensor] = None
+    encoder_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    encoder_attentions: Optional[Tuple[torch.FloatTensor]] = None
+    value: Optional[torch.FloatTensor] = None
+
+
+
+
+def make_head(n_embd: int, out: int, dtype: type = torch.float32) -> nn.Sequential:
+    """Returns a generic sequential MLP head."""
+    return nn.Sequential(
+        nn.Linear(n_embd, n_embd * 2, dtype=dtype),
+        nn.ReLU(),
+        nn.Linear(n_embd * 2, out, dtype=dtype),
+    )
+
+
+def freeze_bottom_causal_layers(model: nn.Module, num_layers_unfrozen: int = 0):
+    """Freezes the bottom transformer block layers of the specified model."""
+    hidden_layers = hf_get_decoder_blocks(model)
+    if num_layers_unfrozen == 0:
+        hidden_layers_to_freeze = list(hidden_layers)
+    elif num_layers_unfrozen > 0:
+        hidden_layers_to_freeze = list(hidden_layers)[:-num_layers_unfrozen]
+    else:
+        hidden_layers_to_freeze = []
+    for layer in hidden_layers_to_freeze:
+        layer.requires_grad_(False)
+
+
+def freeze_bottom_seq2seq_layers(model: nn.Module, num_layers_unfrozen: int = 0):
+    """Freezes the bottom transformer block layers of the specified model."""
+    if num_layers_unfrozen == -1:
+        return
+    shared_embed = model.shared
+    decoder_embed = model.decoder.embed_tokens
+    encoder_blocks = model.encoder.block
+    encoder_norm_layer = model.encoder.final_layer_norm
+    decoder_norm_layer = model.decoder.final_layer_norm
+    decoder_blocks = model.decoder.block[:-num_layers_unfrozen]
+    blocks_to_freeze = (
+        list(encoder_blocks)
+        + list(decoder_blocks)
+        + [shared_embed]
+        + [encoder_norm_layer]
+        + [decoder_norm_layer]
+        + [decoder_embed]
+    )
+    for block in blocks_to_freeze:
+        block.requires_grad_(False)
+
+
+def rhasattr(obj, attr):
+    """A chain-able attribute version of hasattr. For example, to check if
+    `obj` has the attribute `foo.bar.baz`, you can use:
+        `rhasattr(obj, "foo.bar.baz")`
+    Reference: https://stackoverflow.com/a/67303315
+    """
+    _nested_attrs = attr.split(".")
+    _curr_obj = obj
+    for _a in _nested_attrs[:-1]:
+        if hasattr(_curr_obj, _a):
+            _curr_obj = getattr(_curr_obj, _a)
+        else:
+            return False
+    return hasattr(_curr_obj, _nested_attrs[-1])
+
+
+def rgetattr(obj, attr: str, *args) -> object:
+    """A chain-able attribute version of getattr. For example, to get the
+    attribute `foo.bar.baz` from `obj`, you can use:
+        `rgetattr(obj, "foo.bar.baz")`
+    Reference: https://stackoverflow.com/a/31174427
+    """
+
+    def _getattr(obj, attr):
+        return getattr(obj, attr, *args)
+
+    return functools.reduce(_getattr, [obj] + attr.split("."))
+
+
+def findattr(obj, attrs: Tuple[str]) -> Union[object, None]:
+    for attr in attrs:
+        if rhasattr(obj, attr):
+            return rgetattr(obj, attr)
+    raise ValueError(f"Could not find an attribute from `{attrs}` in `{obj}`")
+
+
+def hf_get_decoder(model: nn.Module) -> nn.Module:
+    """Returns the causal decoder backbone of the specified HuggingFace transformers
+    model.
+    NOTE: Different model configurations have different causal decoder attribute
+    names.
+        - transformer: (GPT2LMHeadModel, GPTJConfig)
+        - model.decoder: (OPTConfig, BloomConfig)
+        - gpt_neox: (GPTNeoXConfig)
+    """
+    decoder_attrs = ("transformer", "model.decoder", "gpt_neox", "decoder")
+    return findattr(model, decoder_attrs)
+
+
+def hf_get_decoder_final_norm(model: nn.Module) -> float:
+    """Returns the final (layer) norm of the specified decoder.
+    NOTE: Different model configurations have different final norm attribute names.
+        - transformer.ln_f: (GPT2LMHeadModel, GPTJForCausalLM)
+        - model.decoder.final_layer_norm: (OPTForCausalLM)
+        - gpt_neox.layers.final_layer_norm: (GPTNeoXForCausalLM)
+    """
+    norm_attrs = (
+        "transformer.ln_f",
+        "model.decoder.final_layer_norm",
+        "model.norm",
+        "decoder.final_layer_norm",
+        "gpt_neox.final_layer_norm",
+    )
+    return findattr(model, norm_attrs)
+
+
+def hf_get_decoder_blocks(model: nn.Module) -> Tuple[nn.Module]:
+    """Returns the decoder hidden layers of the specified model.
+    NOTE: Different model configurations have different hidden layer attribute names.
+        - transformer.h: (BloomForCausalLM, GPT2LMHeadModel, GPTJForCausalLM)
+        - model.decoder.layers: (OPTForCausalLM)
+        - gpt_neox.layers: (GPTNeoXForCausalLM)
+        - decoder.block: (T5ForConditionalGeneration)
+    """
+    hidden_layers_attrs = (
+        "h",
+        "layers",
+        "model.layers",
+        "decoder.layers",
+        "transformer.h",
+        "model.decoder.layers",
+        "gpt_neox.layers",
+        "decoder.block",
+    )
+    return findattr(model, hidden_layers_attrs)
+
+
+def hf_get_lm_head(model: nn.Module) -> nn.Module:
+    """Returns the language modeling (lm) head of the specified HuggingFace
+    transformers model.
+    NOTE: Different model configurations have different `lm_head` attribute names.
+        - lm_head: (GPT2LMHeadModel, BloomForCausalLM)
+        - embed_out: (GPTNeoXForCausalLM)
+    """
+    return model.get_output_embeddings()
+
+
+def hf_get_hidden_size(config: PretrainedConfig) -> int:
+    """Returns the hidden layer dimensionality of the model architecture specified
+    by the HuggingFace transformers config.
+    NOTE: Different model configurations have different hidden size attribute names.
+        - hidden_size: (OPTConfig, BloomConfig)
+        - n_embd: (GPT2Config, GPTJConfig)
+        - d_model: (PegasusConfig, XLNetConfig)
+    """
+    hidden_size_attrs = ("hidden_size", "n_embd", "d_model")
+    return findattr(config, hidden_size_attrs)
+
+
+def hf_get_num_hidden_layers(config: PretrainedConfig) -> int:
+    """Returns the number of hidden layers in the model architecture specified
+    by the HuggingFace transformers config.
+    NOTE: Different model configurations have different number-of-layers attribute
+    names.
+        - num_hidden_layers: (GPTNeoXConfig, OPTConfig)
+        - n_layer: (GPT2Config, GPTJConfig, BloomConfig)
+    """
+    num_hidden_layers_attrs = ("num_hidden_layers", "n_layer")
+    return findattr(config, num_hidden_layers_attrs)
+
