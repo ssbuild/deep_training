09ba8ddb34823eb3c20ee39ff6755e1ddc241030
ssbuild <462304@qq.com>
2023-08-04 19:05:54 +0800

    fix qwen
    Signed-off-by: ssbuild <462304@qq.com>

--- src/deep_training/nlp/models/qwen/modeling_qwen.py
+++ src/deep_training/nlp/models/qwen/modeling_qwen.py
@@ -1114,7 +1114,8 @@ class RMSNorm(torch.nn.Module):
         if rms_norm is not None and x.is_cuda:
             return rms_norm(x, self.weight, self.eps)
         else:
-            output = self._norm(x.float()).t
+            output = self._norm(x.float()).type_as(x)
+            return output * self.weight
 
 
 
