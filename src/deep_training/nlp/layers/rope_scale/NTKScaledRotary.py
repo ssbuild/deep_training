38ee7665ddf4e3b96f1b782fbc961c1c49519f4a
ssbuild <462304@qq.cn>
2023-08-16 15:01:59 +0800

    patch rope for chatglm chatglm2
    Signed-off-by: ssbuild <462304@qq.cn>

--- src/deep_training/nlp/layers/rope_scale/NTKScaledRotary.py
+++ src/deep_training/nlp/layers/rope_scale/NTKScaledRotary.py
@@ -32,3 +32,70 @@ class NTKScaledRotary(torch.nn.Module):
             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
         )
+
+
+class NTKScaledRotaryGLM(torch.nn.Module):
+    def __init__(self, dim, base=10000, alpha=1, device=None):
+        super().__init__()
+        base = base * alpha ** (dim / (dim - 2))
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
+        self.register_buffer('inv_freq', inv_freq)
+        self.max_seq_len_cached = None
+
+
+    def forward(self, x, seq_dim=1, seq_len=None):
+        if seq_len is None:
+            seq_len = x.shape[seq_dim]
+        if self.max_seq_len_cached is None or (seq_len > self.max_seq_len_cached):
+            self.max_seq_len_cached = None if self.learnable else seq_len
+            t = torch.arange(seq_len, device=x.device, dtype=self.inv_freq.dtype)
+            freqs = torch.einsum('i,j->ij', t, self.inv_freq)
+            # Different from paper, but it uses a different permutation in order to obtain the same calculation
+            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+            # [sx, 1 (b * np), hn]
+            self.register_buffer("cos_cached", emb.cos()[:, None, ...].to(x.dtype), persistent=False)
+            self.register_buffer("sin_cached", emb.sin()[:, None, ...].to(x.dtype), persistent=False)
+
+        return self.cos_cached[:seq_len, ...], self.sin_cached[:seq_len, ...]
+
+
+
+class NTKScaledRotaryGLM2(torch.nn.Module):
+    def __init__(self, dim,rope_ratio=1.0, original_impl=False,base=10000, alpha=1, device=None):
+        super().__init__()
+        base = base * alpha ** (dim / (dim - 2))
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
+        self.register_buffer('inv_freq', inv_freq)
+        self.dim = dim
+        self.original_impl = original_impl
+        self.rope_ratio = rope_ratio
+
+    def forward_impl(
+            self, seq_len: int, n_elem: int, dtype: torch.dtype, device: torch.device, base: int = 10000
+    ):
+        """Enhanced Transformer with Rotary Position Embedding.
+
+        Derived from: https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/labml_nn/
+        transformers/rope/__init__.py. MIT License:
+        https://github.com/labmlai/annotated_deep_learning_paper_implementations/blob/master/license.
+        """
+        # $\Theta = {\theta_i = 10000^{\frac{2(i-1)}{d}}, i \in [1, 2, ..., \frac{d}{2}]}$
+        theta = 1.0 / (base ** (torch.arange(0, n_elem, 2, dtype=dtype, device=device) / n_elem))
+
+        # Create position indexes `[0, 1, ..., seq_len - 1]`
+        seq_idx = torch.arange(seq_len, dtype=dtype, device=device) / self.rope_ratio
+
+        # Calculate the product of position index and $\theta_i$
+        idx_theta = torch.outer(seq_idx, theta).float()
+
+        cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
+
+        # this is to mimic the behaviour of complex32, else we will get different results
+        if dtype in (torch.float16, torch.bfloat16, torch.int8):
+            cache = cache.bfloat16() if dtype == torch.bfloat16 else cache.half()
+        return cache
+
+    def forward(self, max_seq_len, offset=0):
+        return self.forward_impl(
+            max_seq_len, self.dim, dtype=self.inv_freq.dtype, device=self.inv_freq.device
+        )
