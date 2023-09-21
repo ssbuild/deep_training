f02b9f1aca23993566046339b806cc17d1ba7a6c
ssbuild <462304@qq.cn>
2023-09-21 17:09:10 +0800

    0.2.3
    Signed-off-by: ssbuild <462304@qq.cn>

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 ]
 setup(
     name='deep_training',
-    version='0.2.3.rc0',
+    version='0.2.3',
     description='an easy training architecture',
     long_description='torch_training: https://github.com/ssbuild/deep_training.git',
     license='Apache License 2.0',
